4c9db5b93753e03c9aa6693090af8ad5e6826d73
Cesar52610 <cesar75983696435@gmail.com>
2026-04-28 07:32:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,19 @@ function buscarClima() {
     const cidade = document.getElementById("cidade").value;
     const apiKey = "f2e80ca6bec842e973ef77bf5dcf3695";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`;
-
     if (cidade === "") {
         document.getElementById("resultado").innerHTML = "Digite uma cidade!";
         return;
     }
 
     fetch(url)
-       .then(response => {
+      .then(response => {
             if (!response.ok) {
                 throw new Error("Cidade não encontrada");
             }
             return response.json();
         })
-       .then(data => {
+      .then(data => {
             const temperatura = data.main.temp;
             const descricao = data.weather[0].description;
             const nomeCidade = data.name;
@@ -23,7 +22,7 @@ function buscarClima() {
             document.getElementById("resultado").innerHTML =
                 `${nomeCidade}: ${temperatura}°C, ${descricao}`;
         })
-       .catch(error => {
+      .catch(error => {
             document.getElementById("resultado").innerHTML =
                 `Erro: ${error.message}`;
         });
